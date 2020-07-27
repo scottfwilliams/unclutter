6ac468f9c714c0e2217d0fed4760a752a6a1fa4e
Scott Williams <scottfwilliams@gmail.com>
2020-07-26 20:50:02 -0400

    Fixed find-extensions

--- src/unclutter/find-extensions.py
+++ src/unclutter/find-extensions.py
@@ -3,22 +3,21 @@ import sys
 
 
 def walk_fs(start_dir):
-    print("Extension finder")
-    print("Starting directory is " + start_dir)
+    print("Finding list of unique file extensions under " + start_dir)
     extension_set = set()
     for root, dirs, files in os.walk(start_dir):
-        for filename in files: 
-            filenm, file_extension = os.path.splitext(filename)
-            extension = file_extension.replace(".", "") 
-            # print(extension)
-            extension_set.add(extension) 
-    for ext in extension_set:
+        for filename in files:
+            if "." in filename:
+                filenm, file_extension = os.path.splitext(filename)
+                extension = file_extension.replace(".", "")
+                extension_set.add(extension)
+    sorted_extensions = sorted(list(extension_set))
+    for ext in sorted_extensions:
         print(ext)
 
 
 if __name__ == "__main__":
-    start_dir = "."
+    start_dir_arg = "."
     if len(sys.argv) > 1: 
-        start_dir = sys.argv[1] 
-    walk_fs(start_dir)
-
+        start_dir_arg = sys.argv[1]
+    walk_fs(start_dir_arg)
